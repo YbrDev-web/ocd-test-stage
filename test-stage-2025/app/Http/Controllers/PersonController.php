9737de1e700a483ba7aa4dc32eee6f4a4605b0b4
Ybr.Benromdhane <yacineben2003@gmail.com>
2024-12-24 14:58:30 +0100

    mise en place de la fonction index dans le controller

--- test-stage-2025/app/Http/Controllers/PersonController.php
+++ test-stage-2025/app/Http/Controllers/PersonController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class PersonController extends Controller
 {
-    //
+    /**
+     * Afficher la liste des personnes avec le nom de l'utilisateur qui les a créées.
+     */
+    public function index()
+    {
+        $people = Person::with('creator')->get(); // Récupère les personnes avec leur créateur
+        return view('people.index', compact('people'));
+    }
 }
