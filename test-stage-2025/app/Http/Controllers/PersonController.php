8bfe7cf6a47f23227ddff69d2d0e61fd196f5773
Ybr.Benromdhane <yacineben2003@gmail.com>
2025-01-08 15:00:20 +0100

    suppression de fonction modification dans personController

--- test-stage-2025/app/Http/Controllers/PersonController.php
+++ test-stage-2025/app/Http/Controllers/PersonController.php
@@ -75,28 +75,4 @@ class PersonController extends Controller
             "nb_queries" => count(DB::getQueryLog()),
         ]);
     }
-
-    public function edit($id)
-    {
-        $person = Person::findOrFail($id); // Récupère la personne par ID
-        return view('people.edit', compact('person'));
-    }
-
-    // Mettre à jour la personne
-    public function update(Request $request, $id)
-    {
-        $person = Person::findOrFail($id);
-
-        $validated = $request->validate([
-            'first_name' => 'required|string|max:255',
-            'last_name' => 'required|string|max:255',
-            'birth_name' => 'nullable|string|max:255',
-            'middle_names' => 'nullable|string|max:255',
-            'date_of_birth' => 'nullable|date',
-            'created_by' => 'required|integer|exists:users,id',
-        ]);
-
-        $person->update($validated); // Met à jour les données validées
-        return redirect()->route('people.show', $id)->with('success', 'Les informations ont été mises à jour avec succès.');
-    }
 }
