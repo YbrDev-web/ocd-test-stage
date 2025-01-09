c5a918a29a69870c5426e8d7633da9dd6915ef55
Ybr.Benromdhane <yacineben2003@gmail.com>
2025-01-09 12:19:57 +0100

    configuration du deleteController

--- test-stage-2025/app/Http/Controllers/DeleteController.php
+++ test-stage-2025/app/Http/Controllers/DeleteController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Person;
+
 
 class DeleteController extends Controller
-{
-    //
+{    
+    public function delete($id)
+    {
+        $person = Person::find($id); // Récupère la personne par ID
+        
+        if(!$person){
+            return response()->json(['message' => 'personne non trouvée.'], 404);
+        }
+
+        $person->delete();
+
+        return response()->json(['message' => 'personne supprimée.'], 200);
+    }
 }
